7818eeffff734eefc85d0d44982421f2615a5175
luk3ya0 <oneTOinf@163.com>
2022-10-09 11:00:18 +0800

    fix some env var and skhd working around yabai

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -226,23 +226,20 @@ fi
 [ -f "$HOME/.config/zsh/local.zsh" ] && source "$HOME/.config/zsh/local.zsh"
 [ -f "$HOME/.config/zsh/fzf-keybind.zsh" ] && source "$HOME/.config/zsh/fzf-keybind.zsh"
 
-
-
-
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$('/Users/luke/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-# if [ $? -eq 0 ]; then
-#     eval "$__conda_setup"
-# else
-#     if [ -f "/Users/luke/miniforge3/etc/profile.d/conda.sh" ]; then
-#         . "/Users/luke/miniforge3/etc/profile.d/conda.sh"
-#     else
-#         export PATH="/Users/luke/miniforge3/bin:$PATH"
-#     fi
-# fi
-# unset __conda_setup
+__conda_setup="$('/Users/luke/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/luke/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/luke/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/luke/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
 # <<< conda initialize <<<
 
--- zsh/local.zsh
+++ zsh/local.zsh
@@ -9,12 +9,13 @@ export TERM=xterm-256color
 export no_proxy=localhost,127.0.0.1,localaddress,.localdomain.com,www.bilibili.com,pub.flutter-io.cn,storage.flutter-io.cn
 
 # flink environment
-export FLINK_HOME="/Library/Flink/flink-1.10.0"
+# export FLINK_HOME="/Library/Flink/flink-1.10.0"
 
 # java environment
-# export JAVA_HOME=$(/usr/libexec/java_home)
-# export CLASSPATH=.:$JAVA_HOME/lib:$FLINK_HOME/lib # :$JRE_HOME/lib
-# export PATH=$PATH:$JAVA_HOME/bin:$FLINK_HOME/bin  # :$JRE_HOME/bin
+# export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+export PATH="$PATH:$JAVA_HOME/bin"
+export CLASSPATH=".:$JAVA_HOME/lib"
 # maven environment
 # export MAVEN_HOME="/opt/homebrew/Cellar/maven/3.8.1"
 # export PATH=$PATH:$MAVEN_HOME/bin
@@ -25,7 +26,7 @@ export FLINK_HOME="/Library/Flink/flink-1.10.0"
 
 # go env
 export GO111MODULE="on"
-export GOROOT="/usr/local/go"
+export GOROOT="/opt/homebrew/Cellar/go/1.19/libexec"
 export GOBIN=$GOROOT/bin
 export GOARCH=arm64
 export GOPATH=$HOME/.gopher
@@ -58,23 +59,21 @@ alias ctags=/usr/local/bin/ctags
 # homebrew
 export PATH="/opt/homebrew/bin:$PATH"
 
-# using GNU date
-export PATH="/opt/homebrew/Cellar/coreutils/9.0_1/bin:$PATH"
-
 # using MaTex
-export PATH="/usr/local/texlive/2021/bin/universal-darwin:$PATH"
+export PATH="/usr/local/texlive/2022/bin/universal-darwin:$PATH"
 
-export DOCKER_HOST=ssh:luke@192.168.3.7
-
-export PKG_CONFIG_PATH=/opt/homebrew/Cellar/zlib/1.2.11/lib/pkgconfig:/opt/homebrew/lib/pkgconfig
+export DOCKER_HOST=ssh:luke@10.0.1.5
 
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
 
 export PATH=/Applications/Hammerspoon.app/Contents/Frameworks/hs:$PATH
 
-export PATH="/usr/local/mysql/bin:$PATH"
+# export PATH="/usr/local/mysql/bin:$PATH"
 
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
-export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+export CC="/opt/homebrew/Cellar/gcc/12.1.0/bin/gcc-12"
+export CXX="/opt/homebrew/Cellar/gcc/12.1.0/bin/gcc-12"
+
+export PATH="/Applications/Hammerspoon.app/Contents/Frameworks/hs:$PATH"
