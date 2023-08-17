146e8860b12e78140bf2f2c5c9033c233156b8b4
luk3ya0 <oneTOinf@163.com>
2023-08-17 16:41:28 +0800

    update alias for zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,29 +1,12 @@
-######################## personal aliases ############################
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-alias zshconfig="vim ~/.zshrc"
-alias ohmyzsh="vim ~/.oh-my-zsh"
-alias cls='colorls'
-alias ping='prettyping -c 10'
-alias vi="nvim"
-alias du='ncdu'
-alias pip3='python3 -m pip'
-alias tmux="TERM=screen-256color-bce tmux"
-alias dops="docker ps --format='table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Ports}}'"
-alias urlen='python3 -c "import sys, urllib.parse as ulp; print(ulp.quote(sys.argv[1]))"'
-alias urlde='python3 -c "import sys, urllib.parse as ulp; print(ulp.unquote(sys.argv[1]))"'
-alias nadb="/Applications/Nox\ App\ Player.app/Contents/MacOS/adb"
-alias vi='nvim'
-alias vim='nvim'
-alias ls='ls -G --color'
-alias drawio='/Applications/draw.io.app/Contents/MacOS/draw.io'
+alias 'clash'='nohup /opt/clash/clash -f ~/.config/clash/config.yaml -d ~/.config/clash > /dev/null 2>&1 &'                  
+alias 'unclash'='pkill -9 clash'
+
+alias proxy="export http_proxy=http://127.0.0.1:7890;export https_proxy=http://127.0.0.1:7890;export socks_proxy=socks5://127.0.0.1:7891"
+alias inetproxy="export http_proxy=http://10.0.1.2:1082;export https_proxy=http://10.0.1.2:1082;export socks_proxy=socks5://10.0.1.2:1082"
+alias unproxy="unset http_proxy;unset https_proxy"
+alias vim="nvim"
+
+alias grep="grep -v grep | grep"
 
 alias setclip="xclip -selection c"
 alias getclip="xclip -selection c -o"
-
-# alias go='http_proxy=http://127.0.0.1:1087/ https_proxy=http://127.0.0.1:1087/ no_proxy=localhost,127.0.0.0/8,::1 go'
-
