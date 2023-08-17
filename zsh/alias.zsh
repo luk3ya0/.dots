add66bb196c23685e1c87c823dffa657fd015d90
luk3ya0 <oneTOinf@163.com>
2023-08-17 20:08:05 +0800

    update alias for zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,12 +1,10 @@
-alias 'clash'='nohup /opt/clash/clash -f ~/.config/clash/config.yaml -d ~/.config/clash > /dev/null 2>&1 &'                  
-alias 'unclash'='pkill -9 clash'
-
 alias proxy="export http_proxy=http://127.0.0.1:7890;export https_proxy=http://127.0.0.1:7890;export socks_proxy=socks5://127.0.0.1:7891"
 alias inetproxy="export http_proxy=http://10.0.1.2:1082;export https_proxy=http://10.0.1.2:1082;export socks_proxy=socks5://10.0.1.2:1082"
-alias unproxy="unset http_proxy;unset https_proxy"
-alias vim="nvim"
 
-alias grep="grep -v grep | grep"
+alias unproxy="unset http_proxy;unset https_proxy;unset socks_proxy"
 
 alias setclip="xclip -selection c"
 alias getclip="xclip -selection c -o"
+
+alias vim="nvim"
+alias grep="grep -v grep | grep"
